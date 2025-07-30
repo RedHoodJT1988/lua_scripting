82d48138884e1e1c2416635c12855b70c2d78e83
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2025-07-29 21:40:12 -0500

    completed salary increase challenge

---
+++ salary-increase.lua
@@ -0,0 +1,19 @@
+-------------------------------------
+-- Exercise:
+-------------------------------------
+-- Wirte a Lua program that receives
+-- the value of the name and salary
+-- of the user.
+-- The proram should calculate and
+-- display the new salary increased
+-- by 15%.
+------------------------------------
+
+name = "Tommy Oliver"
+salary = 500000.00
+
+-----------------------------------
+---TODO: calculate new_salary
+-----------------------------------
+new_salary = salary +salary * (15/100)
+print("New Salary is: " .. new_salary)
