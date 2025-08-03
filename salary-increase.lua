9c3b41ef35179dc530e5d064d59cd50494034005
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2025-08-03 16:00:00 -0500

    updated previous challenge

--- salary-increase.lua
+++ salary-increase.lua
@@ -1,19 +1,9 @@
--------------------------------------
--- Exercise:
--------------------------------------
--- Wirte a Lua program that receives
--- the value of the name and salary
--- of the user.
--- The proram should calculate and
--- display the new salary increased
--- by 15%.
-------------------------------------
-
 name = "Tommy Oliver"
 salary = 500000.00
 
------------------------------------
----TODO: calculate new_salary
------------------------------------
 new_salary = salary +salary * (15/100)
 print("New Salary is: " .. new_salary)
+
+-- An alternate approach to solve the challenge is:
+new_salary = salary * 1.15
+print("The salary for " ..name.. " after 15% increase is: " ..new_salary)
